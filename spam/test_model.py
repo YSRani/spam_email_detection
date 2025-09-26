f1ea69ff2a61bd851f5017f5c46207933dfafa6e
Sandhya rani yalla <143862947+YSRani@users.noreply.github.com>
2025-09-26 11:13:00 +0530

    Delete spam directory

--- spam/test_model.py
+++
@@ -1,8 +0,0 @@
-import pickle
-
-with open("nb_model.pkl", "rb") as f:
-    try:
-        nb_model = pickle.load(f)
-        print("✅ Model loaded successfully!")
-    except Exception as e:
-        print("❌ Error loading model:", e)
